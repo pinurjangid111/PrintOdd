3c6468e52df1248b8f33484266d1fa0a17530873
Krishna Jangid <72332320+pinurjangid111@users.noreply.github.com>
2022-02-16 11:59:20 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Odd{
+public static void main(String []args){
+System.out.println("ODD are :");
+for(int i=1;i<=100;i++)
+{
+if(i%2 !=0){
+System.out.prin(i+ " ");
+}
+}
+}
+}
